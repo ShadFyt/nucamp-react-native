09f7f5e0d5122a7a33fa597d52065af87202aacd
ShadFyt <doanryan10@gmail.com>
2021-11-18 20:07:30 -0600

    Activity Indicato

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { Card, ListItem } from 'react-native-elements'
 import { connect } from 'react-redux';
 
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 function Mission() {
     return (
@@ -29,7 +30,7 @@ class About extends Component {
     }
 
     render() {
-        function renderPartner({ item }) {
+        const renderPartner = ({ item }) => {
             return (
                 <ListItem
                     title={item.name}
@@ -39,6 +40,28 @@ class About extends Component {
             )
         }
 
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            )
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            )
+        }
         return (
             <ScrollView>
                 <Mission />
