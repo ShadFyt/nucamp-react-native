9b65568dddd779e41fa022d5b07510c723c3d051
ShadFyt <doanryan10@gmail.com>
2021-11-23 22:14:44 -0600

    animatable

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -12,6 +12,7 @@ import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { postFavorite, postComment } from '../redux/ActionCreators';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -42,40 +43,44 @@ function RenderComments({ comments }) {
         )
     }
     return (
-        <Card title="Comments">
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={(item) => item.id.toString()}
-            />
-        </Card>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+            <Card title="Comments">
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={(item) => item.id.toString()}
+                />
+            </Card>
+        </Animatable.View>
     )
 }
 
 function RenderCampsite({ campsite, favorite, markFavorite, onShowModal }) {
     if (campsite) {
         return (
-            <Card featuredTitle={campsite.name} image={{ uri: baseUrl + campsite.image }}>
-                <Text style={{ margin: 10 }}>{campsite.description}</Text>
-                <View style={styles.cardRow}>
-                    <Icon
-                        name={campsite.favorite ? "heart" : "heart-o"}
-                        type="font-awesome"
-                        color="#f50"
-                        raised
-                        reverse
-                        onPress={() => favorite ? console.log("already set as favorite") : markFavorite()}
-                    />
-                    <Icon
-                        name="pencil"
-                        type="font-awesome"
-                        color="#5637DD"
-                        raised
-                        reverse
-                        onPress={() => onShowModal()}
-                    />
-                </View>
-            </Card>
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card featuredTitle={campsite.name} image={{ uri: baseUrl + campsite.image }}>
+                    <Text style={{ margin: 10 }}>{campsite.description}</Text>
+                    <View style={styles.cardRow}>
+                        <Icon
+                            name={campsite.favorite ? "heart" : "heart-o"}
+                            type="font-awesome"
+                            color="#f50"
+                            raised
+                            reverse
+                            onPress={() => favorite ? console.log("already set as favorite") : markFavorite()}
+                        />
+                        <Icon
+                            name="pencil"
+                            type="font-awesome"
+                            color="#5637DD"
+                            raised
+                            reverse
+                            onPress={() => onShowModal()}
+                        />
+                    </View>
+                </Card>
+            </Animatable.View>
         )
     }
     return (<View />)
