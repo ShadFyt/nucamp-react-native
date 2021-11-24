80a6045e44b7f169c7c269854538a3dae2977297
ShadFyt <doanryan10@gmail.com>
2021-11-23 21:34:20 -0600

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -13,6 +13,7 @@ import Home from "./HomeComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import Reservation from "./ReservationComponent";
+import Favorites from './FavoritesComponent';
 import { connect } from 'react-redux';
 import {
     fetchCampsites, fetchComments, fetchPromotions,
@@ -147,6 +148,29 @@ const ReservationNavigator = createStackNavigator(
     }
 );
 
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites }
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView
@@ -234,12 +258,27 @@ const MainNavigator = createDrawerNavigator(
                     />
                 )
             }
-        }
+        },
+        Favorites: {
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({ tintColor }) => (
+                    <Icon
+                        name='heart'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
     },
     {
         drawerBackgroundColor: '#CEC8FF',
         contentComponent: CustomDrawerContentComponent
-    }
+    },
+
 )
 
 const AppNavigator = createAppContainer(MainNavigator);
