b9330d3f9a8787e3044cc868ac7d47c76a4a9311
ShadFyt <doanryan10@gmail.com>
2021-12-01 22:08:01 -0600

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -5,6 +5,7 @@ import {
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 
 class Reservation extends Component {
@@ -35,6 +36,33 @@ class Reservation extends Component {
         });
     }
 
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            })
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            })
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
+
     createAlert = () => {
         Alert.alert(
             "Begin Search?",
@@ -48,7 +76,10 @@ class Reservation extends Component {
                 },
                 {
                     text: "OK",
-                    onPress: () => this.resetForm()
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString("en-US"));
+                        this.resetForm();
+                    }
                 },
             ]
         )
